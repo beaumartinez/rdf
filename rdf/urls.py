34f3d5f2635260968554217b033e8b937cbebe87
Beau <beau@beaumartinez.com>
2012-08-30 15:54:06 +0100

    Add log_in and log_out URLs

--- rdf/urls.py
+++ rdf/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('rdf.views',
     url(r'^$', 'home', name='home'),
 
+    url('^log-out/$', 'log_out', name='log_out'),
+    url('^log-in/$', 'log_in', name='log_in'),
+
     url('^twitter-oauth/request/$', 'twitter_oauth_request', name='twitter_oauth_request'),
     url('^twitter-oauth/verify/$', 'twitter_oauth_verify', name='twitter_oauth_verify'),
 )
