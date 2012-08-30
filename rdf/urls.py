d76bea9ef7d324278aed858a8615da2402c183f7
Beau <beau@beaumartinez.com>
2012-08-30 20:06:58 +0100

    Add landing URL

--- rdf/urls.py
+++ rdf/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('rdf.views',
-    url(r'^$', 'home', name='home'),
+    url(r'^$', 'landing', name='landing'),
+
+    url(r'^home/$', 'home', name='home'),
 
     url('^log-out/$', 'log_out', name='log_out'),
     url('^log-in/$', 'log_in', name='log_in'),
