5e319b44bd07a9d76e8834476883da80aa3e2aac
Beau <beau@beaumartinez.com>
2012-08-30 15:49:33 +0100

    Add Twitter auth URLs

--- rdf/urls.py
+++ rdf/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('rdf.views',
     url(r'^$', 'home', name='home'),
+
+    url('^twitter-oauth/request/$', 'twitter_oauth_request', name='twitter_oauth_request'),
+    url('^twitter-oauth/verify/$', 'twitter_oauth_verify', name='twitter_oauth_verify'),
 )
