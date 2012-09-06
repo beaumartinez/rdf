b3133009cd8cf8ccf5611dc10cc2ea1f61fe4b9b
Beau <beau@beaumartinez.com>
2012-09-06 21:07:04 +0100

    Add retweet functions to utils.py

--- rdf/utils.py
+++ rdf/utils.py
@@ -1,5 +1,34 @@
+from random import randrange
+
 from django.conf import settings
-from tweepy import API, OAuthHandler
+from tweepy import API, OAuthHandler, TweepError
+
+def _random_favorite_tweet(api):
+    twitter_user = api.me()
+
+    favorites_count = twitter_user.favourites_count
+
+    page = randrange(favorites_count)
+
+    tweet = api.favorites(count=1, page=page)
+    tweet = tweet[0]
+
+    return tweet
+
+def retweet_random_favorite_tweet(api):
+    retweeted = False
+
+    while not retweeted:
+        tweet = _random_favorite_tweet(api)
+
+        try:
+            tweet.retweet()
+        except TweepError:
+            pass
+        else:
+            retweeted = True
+
+    return tweet
 
 def user_api(user):
     profile = user.get_profile()
