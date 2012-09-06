6d4241270a524c7d6849d3c3244b053dd7b7c47d
Beau <beau@beaumartinez.com>
2012-09-06 16:23:50 +0100

    Actually use RetweetManager

--- rdf/models.py
+++ rdf/models.py
@@ -7,6 +7,13 @@ from django.db.models import BooleanField, CharField, DateTimeField, ForeignKey,
 
 from rdf.constants import MAX_FREQUENCY, MIN_FREQUENCY, SECONDS_PER_DAY
 
+class RetweetManager(Manager):
+
+    def last_five(self):
+        return self[:5]
+
+# Models
+
 class Retweet(Model):
     datetime = DateTimeField(default=datetime.utcnow)
     profile = ForeignKey('UserProfile', related_name='retweets')
@@ -15,7 +22,7 @@ class Retweet(Model):
     class Meta(object):
         ordering = ('-datetime',)
 
-    objects = 'RetweetManager'
+    objects = RetweetManager()
 
 class Settings(Model):
     frequency = PositiveIntegerField(default=MIN_FREQUENCY, help_text='How many times to retweet per day', validators=[MaxValueValidator(MAX_FREQUENCY), MinValueValidator(MIN_FREQUENCY)])
@@ -42,12 +49,5 @@ class UserProfile(Model):
             next_retweet_ = last_retweet.datetime + relativedelta(seconds=seconds)
         
         return next_retweet_
-
-# Managers
-
-class RetweetManager(Manager):
-
-    def last_five(self):
-        return self[:5]
 
 import rdf.signals
