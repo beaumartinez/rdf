15fc33879c154c5ca3c1dc1f21c8192717f6421b
Beau <beau@beaumartinez.com>
2012-09-10 20:26:50 +0100

    Add RetweetTask

--- rdf/tasks.py
+++ rdf/tasks.py
@@ -1,10 +1,20 @@
-from celery import task
+from celery import Task, task
 from celery.task.control import revoke
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 from rdf.models import Retweet, Settings, UserProfile
 from rdf.utils import user_api, retweet_random_favorite_tweet
+
+class RetweetTask(Task):
+
+    abstract = True
+
+    def after_return(self, *args, **kwargs):
+        user_name = args[0]
+        profile = UserProfile.objects.get(user__username=user_name)
+
+        self.apply_async(args, eta=profile.next_retweet)
 
 @task
 def retweet(user_name):
